0a1339bc5f0b1f5f6f263da92dd3d3ddecc8c0bc
comradescallion <schmoogalooga@gmail.com>
2024-02-20 18:18:32 -0500

    oh we main

--- game/MalpracticeMakesPerfect/Slot.cs
+++ game/MalpracticeMakesPerfect/Slot.cs
@@ -11,10 +11,26 @@ namespace MalpracticeMakesPerfect
         private Item item;
         private int amount;
 
+
+        public Slot()
+        {
+            item = null;
+            amount = 0;
+        }
+
         public Slot(Item item, int amount)
         {
             this.item = item;
             this.amount = amount;
+        }
+
+        public void AddItem(Item item, int amount)
+        {
+            if (this.item != null)
+            {
+                this.item = item;
+                this.amount = amount;
+            }
         }
     }
 }
