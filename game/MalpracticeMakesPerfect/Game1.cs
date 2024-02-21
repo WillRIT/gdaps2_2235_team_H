93968e9c6f0d9258dee8dbaad8b5f107e5fef507
Katherine Fishbane <kf1634@rit.edu>
2024-02-21 13:34:17 -0500

    Bare bones menu done

--- game/MalpracticeMakesPerfect/Game1.cs
+++ game/MalpracticeMakesPerfect/Game1.cs
@@ -4,21 +4,35 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MalpracticeMakesPerfect
 {
+    /// <summary>
+    /// Contains the game's mode
+    /// </summary>
+    enum GameMode
+    {
+        Menu, Game, GameOver
+    }
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D jam;
         private Texture2D Joobi;
+        private SpriteFont menuFont;
 
         private Rectangle item;
 
+        //KeyboardStates
+        private KeyboardState currentKbState;
+        private KeyboardState previousKbState;
 
+        //GameMode
+        private GameMode currentState;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            currentState = GameMode.Menu;
         }
 
         protected override void Initialize()
@@ -31,7 +45,7 @@ namespace MalpracticeMakesPerfect
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            menuFont = Content.Load<SpriteFont>("MenuFont");
 
             // TODO: use this.Content to load your game content here
         }
@@ -40,9 +54,20 @@ namespace MalpracticeMakesPerfect
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            currentKbState = Keyboard.GetState();
+           
+            //switches between GameModes
+            switch (currentState)
+            {
+                case GameMode.Menu:
+                    if (SingleKeyPress(Keys.Space, currentKbState))
+                    {
+                        currentState = GameMode.Game;
+                    }
+                    break;
+            }
 
-            // TODO: Add your update logic here
-
+            previousKbState = currentKbState;
             base.Update(gameTime);
         }
 
@@ -50,9 +75,37 @@ namespace MalpracticeMakesPerfect
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            
+            
+            switch (currentState)
+            {
+                //Main Menu Text
+                case GameMode.Menu:
+                    _spriteBatch.DrawString(menuFont, "Malpractice Makes Perfect", new Vector2(20, 20), Color.Black);
+                    _spriteBatch.DrawString(menuFont, "Team Borderline Doctors", new Vector2(20, 50), Color.Black);
+                    _spriteBatch.DrawString(menuFont, "Press space to begin", new Vector2(20, 70), Color.Black);
 
+                    break;
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Checks for a single key press
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="kbState"></param>
+        /// <returns></returns>
+        private bool SingleKeyPress(Keys key, KeyboardState kbState)
+        {
+            kbState = Keyboard.GetState();
+            if (kbState.IsKeyDown(key) && previousKbState.IsKeyUp(key))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
